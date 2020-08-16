9de10ed9c1c8e866dea47c708a242a9a93bc6755
ebertmota <ebert.mota03@gmail.com>
2020-08-15 21:04:54 -0300

    finishing login

---
+++ mobile/src/pages/Dashboard/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Text, Button } from 'react-native';
+
+import { useAuth } from '../../hooks/auth';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
+  return (
+    <Container>
+      <Text>Dashboard</Text>
+      <Button title="sair" onPress={signOut} />
+    </Container>
+  );
+};
+
+export default Dashboard;
