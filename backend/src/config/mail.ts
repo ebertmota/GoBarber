4a72b707047cf0444dd0276b39c191470b43540d
ebertmota <ebert.mota03@gmail.com>
2020-09-29 22:59:22 -0300

    sending emails with amazon ses

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'ebert@omota.dev',
+      name: 'Ebert da GoBarber',
+    },
+  },
+} as IMailConfig;
